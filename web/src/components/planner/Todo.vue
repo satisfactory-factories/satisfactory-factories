75db8dfa1aaeeb55c49ff7eddb599a801ad30b6b
Matt Cavanagh <1776058+Maelstromeous@users.noreply.github.com>
2024-10-13 00:52:46 +0100

    feat: Updated todo

--- web/src/components/planner/Todo.vue
+++ web/src/components/planner/Todo.vue
@@ -3,18 +3,17 @@
     <v-col>
       <v-card>
         <v-card-title>Todo</v-card-title>
-        <v-card-text>
-          <v-list density="compact">
-            <v-list-item>Bug: Delete group deletes wrong factories!</v-list-item>
-            <v-list-item>Bug: When a group is deleted, the dependants are not properly updated. Need to check if
-              inputs are still valid and if not delete them.
-            </v-list-item>
-            <v-list-item>Feat: If user attempts to set an output which has dependants to sink, warn the user
+        <v-card-text class="text-body-1">
+          <ul class="ml-4">
+            <li>TEST ALL THE THINGS!!!</li>
+            <li>Bug: When a group is deleted, the dependants are not properly updated. Need to check if inputs are still valid and if not delete them.
+            </li>
+            <li>Feat: If user attempts to set an output which has dependants to sink, warn the user
               first because it's potentially destructive as we remove the invalid inputs.
-            </v-list-item>
-            <v-list-item>Feat: Complete request bar segments.</v-list-item>
-            <v-list-item>Feat: Enable to sink remainder, or sink entire output.</v-list-item>
-          </v-list>
+            </li>
+            <li>Feat: Complete request bar segments.</li>
+            <li>Feat: Enable to sink remainder, or sink entire output.</li>
+          </ul>
         </v-card-text>
       </v-card>
     </v-col>
