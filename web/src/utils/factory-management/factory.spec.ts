804ef4e4cb337f6621cd57d34ca515238dff1ef3
Matt Cavanagh <1776058+Maelstromeous@users.noreply.github.com>
2024-10-27 16:33:35 +0100

    feat: Add tests for new factory

---
+++ web/src/utils/factory-management/factory.spec.ts
@@ -0,0 +1,13 @@
+import { newFactory } from '@/utils/factory-management/factory'
+import { describe, expect, it } from '@jest/globals'
+
+describe('Factory Management', () => {
+  describe('newFactory', () => {
+    it('should create a new factory with a random ID', () => {
+      const fac = newFactory('My new factory')
+      expect(fac.id).toBeGreaterThan(0)
+      expect(fac.name).toBe('My new factory')
+      expect(fac.products.length).toBe(0)
+    })
+  })
+})
