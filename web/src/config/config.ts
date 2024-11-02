c94b7dee498895e470fb34d1c66e8f64ab8711df
Matt Cavanagh <1776058+Maelstromeous@users.noreply.github.com>
2024-11-02 13:12:59 +0000

    feat: Clipped the config condition around, it seems process.env is not populated by vercel at runtime

--- web/src/config/config.ts
+++ web/src/config/config.ts
@@ -1,4 +1,4 @@
 export const config = {
-  apiUrl: process.env.ENVIRONMENT === 'production' ? 'https://api.satisfactory-factories.app' : 'http://localhost:3001',
+  apiUrl: process.env.ENVIRONMENT === 'dev' ? 'http://localhost:3001' : 'https://api.satisfactory-factories.app',
   dataVersion: '1.0-10',
 }
