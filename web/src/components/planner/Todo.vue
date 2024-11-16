c899efb74c1c88820e2b1b847a0b797ae2ec0b7f
Matt Cavanagh <1776058+Maelstromeous@users.noreply.github.com>
2024-11-16 19:33:35 +0000

    feat: Updated todo and restyled buttons

--- web/src/components/planner/Todo.vue
+++ web/src/components/planner/Todo.vue
@@ -2,17 +2,17 @@
   <v-row>
     <v-col>
       <v-card>
-        <v-card-title>Upcoming features / known bugs</v-card-title>
+        <v-card-title><h1 class="text-h5">Upcoming features / known bugs</h1></v-card-title>
         <v-card-text class="text-body-1">
           <p class="mb-2">
-            Please report any bugs or feature requests to <a href="https://github.com/Maelstromeous/satisfactory-factories/issues"><i class="fab fa-github mr-1" />SF's GitHub page</a> or <a href="https://www.reddit.com/r/SatisfactoryGame/comments/1gsm980/new_tool_satisfactory_factories_a_production/">Reddit post</a>!
+            Please report any bugs or feature requests to <a class="font-weight-bold text-blue" href="https://github.com/Maelstromeous/satisfactory-factories/issues"><i class="fab fa-github mr-1" />SF's GitHub page</a> or <a class="text-blue" href="https://www.reddit.com/r/SatisfactoryGame/comments/1gsm980/new_tool_satisfactory_factories_a_production/">Reddit post</a>!
           </p>
           <ul class="ml-4">
-            <li><span class="text-amber">Potential bug</span>: Certain fluid recipes could be inaccurate. Please offer feedback!</li>
+            <li><span class="text-red">Bug:</span> Quantum Encoder has no power values.</li>
+            <li><span class="text-red">Bug:</span> Imports are not showing up for collapsed / hidden factories.</li>
+            <li><span class="text-red">Bug:</span> Missing internal chip next to byproducts that are used internally.</li>
             <li>Feat: <b>Overclocking / Somersloops</b> (WIP, omitted for Alpha release, it's a LOT of math!)</li>
             <li>Feat: <b>Quantum Converter support</b> (omitted for Alpha release), it's a surprisingly complex thing to get right...</li>
-            <li>Bug: Imports are not showing up for collapsed / hidden factories.</li>
-            <li>Bug: Missing internal chip next to byproducts that are used internally.</li>
             <li>Feat: Add ability to add icon to factory.</li>
             <li>Feat: Allow ability to add duplicate products to a factory (in case you split processing of them up).</li>
             <li>Feat: Show unsatisfied demands for factories for easy creation of new factories.</li>
@@ -23,7 +23,7 @@
             <li>Feat: World Resources: Show % used.</li>
             <li>Feat: Add button to jump to the node in the graph.</li>
             <li>Feat: Password resets (use a password manager people! 🔐)</li>
-            <li><span class="text-green">FIXED - Critical bug</span>: Recipes producing multiples of the same item e.g. Caterium CBs producing 7, are being regarded as producing 1 and the part demand not being ratioed correctly. Thanks to /u/walktheplank-yohoho for the report! <span class="text-green">Fix released 14:45 UTC 16th Nov</span>, please refresh and report any further issues! <a class="text-cyan" href="https://github.com/Maelstromeous/satisfactory-factories/issues/1">Please report any broken recipes here!</a> </li>
+            <li><span class="text-green">FIXED 14:45 UTC Nov 16th - Critical bug</span>: Products where recipes created multiple items multiplied ingredients by the number of products per recipe cycle.</li>
           </ul>
         </v-card-text>
       </v-card>
