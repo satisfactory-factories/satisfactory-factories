bfd612973c9244f87fa504e4863cb5046ba941b1
Matt Cavanagh <1776058+Maelstromeous@users.noreply.github.com>
2024-11-16 19:33:35 +0000

    feat: Added bad bug

--- web/src/components/planner/Todo.vue
+++ web/src/components/planner/Todo.vue
@@ -8,11 +8,14 @@
             Please report any bugs or feature requests to <a class="font-weight-bold text-blue" href="https://github.com/Maelstromeous/satisfactory-factories/issues"><i class="fab fa-github mr-1" />SF's GitHub page</a> or <a class="text-blue" href="https://www.reddit.com/r/SatisfactoryGame/comments/1gsm980/new_tool_satisfactory_factories_a_production/">Reddit post</a>!
           </p>
           <ul class="ml-4">
+            <li><span class="text-red font-weight-bold">Major Bug:</span> Multiple products of the same item using different recipes will only be counted once (the last entry).</li>
             <li><span class="text-red">Bug:</span> Quantum Encoder has no power values.</li>
             <li><span class="text-red">Bug:</span> Imports are not showing up for collapsed / hidden factories.</li>
             <li><span class="text-red">Bug:</span> Missing internal chip next to byproducts that are used internally.</li>
             <li>Feat: <b>Overclocking / Somersloops</b> (WIP, omitted for Alpha release, it's a LOT of math!)</li>
             <li>Feat: <b>Quantum Converter support</b> (omitted for Alpha release), it's a surprisingly complex thing to get right...</li>
+            <li>Bug: Imports are not showing up for collapsed / hidden factories.</li>
+            <li>Bug: Missing internal chip next to byproducts that are used internally.</li>
             <li>Feat: Add ability to add icon to factory.</li>
             <li>Feat: Allow ability to add duplicate products to a factory (in case you split processing of them up).</li>
             <li>Feat: Show unsatisfied demands for factories for easy creation of new factories.</li>
