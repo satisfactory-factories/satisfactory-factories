9ff76e230b933c5aa2598ab02a7a170b30fa75ca
Sam Smith <samsmithnz@github.com>
2024-11-17 13:09:43 +0000

    fix to number formatter

--- web/src/utils/numberFormatter.ts
+++ web/src/utils/numberFormatter.ts
@@ -1,7 +1,9 @@
 export function formatNumber(value: any): string {
   const num = Number(value);
   if (isNaN(num)) {
-    throw new TypeError('The provided value is not a number');
+    //throw new TypeError('The provided value is not a number');
+    //Instead of an error - just return the value as is. This happens with the totalPower (todo: why is this a string and not a number?)
+    return value;
   }
   return num % 1 === 0 ? num.toFixed(0) : parseFloat(num.toFixed(2)).toString();
 }
