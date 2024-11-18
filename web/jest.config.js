10e1f5559fc053a872f5983c59bd55704423f704
Matt Cavanagh <1776058+Maelstromeous@users.noreply.github.com>
2024-11-18 17:23:15 +0000

    fix(web): Fixed wonky wording and erroneous stuff in coverage collectors

--- web/jest.config.js
+++ web/jest.config.js
@@ -8,7 +8,6 @@ module.exports = {
   collectCoverageFrom: [
     '**/*.{ts,tsx}', // Include only TypeScript and TSX files
     '**/*.spec.ts', // Include test files
-    '**/*index.ts', // Exclude index files if they just re-export, as an example
     '!src/config/**', // Exclude config folder, mostly pointless to test
     '!**/*.module.ts', // Exclude module files, really hard to test with not much value
     '!**/*.d.ts', // Exclude declaration files, as they're not needed for testing
