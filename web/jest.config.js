88682a72d49426de4b71ff884d1365ad12368e4e
Matt Cavanagh <1776058+Maelstromeous@users.noreply.github.com>
2024-11-18 17:13:31 +0000

    feat(web): Set up test code coverages #57

--- web/jest.config.js
+++ web/jest.config.js
@@ -4,4 +4,17 @@ module.exports = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}', // Include only TypeScript and TSX files
+    '**/*.spec.ts', // Explicitly exclude test files
+    '**/*index.ts', // Exclude index files if they just re-export, as an example
+    '!src/config/**', // Exclude config folder, mostly pointless to test
+    '!**/*.module.ts', // Exclude module files, really hard to test with not much value
+    '!**/*.d.ts', // Exclude declaration files, as they're not needed for testing
+    '!src/router/**', // Exclude router folder, as it's mostly just a bunch of routes supplied by Vue3
+    '!main.ts', // Exclude main.ts, as it's the entrypoint to the app
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['json-summary', 'text', 'lcov'],
 }
