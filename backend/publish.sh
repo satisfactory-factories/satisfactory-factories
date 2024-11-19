00b30150cdda9ad71c91d0cbbe3cd57e0635d56e
Matt Cavanagh <1776058+Maelstromeous@users.noreply.github.com>
2024-11-19 21:43:06 +0000

    feat(backend): Updated publish script to invoke a command on personal dev server to pull in the image and deploy it

--- backend/publish.sh
+++ backend/publish.sh
@@ -3,3 +3,5 @@ docker build --platform linux/amd64 . -t sf-backend
 # Push to ghcr.io
 docker tag sf-backend ghcr.io/maelstromeous/satisfactory-factories:backend-latest
 docker push ghcr.io/maelstromeous/satisfactory-factories:backend-latest
+
+ssh ceres "./publish-sf-backend.sh"
