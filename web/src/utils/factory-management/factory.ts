e45950f7dc1da58f54ca263a0c57ab463835a3f8
Matt Cavanagh <1776058+Maelstromeous@users.noreply.github.com>
2024-11-20 13:09:56 +0000

    feat(web): Gutted demoFactories and made it programmatic

--- web/src/utils/factory-management/factory.ts
+++ web/src/utils/factory-management/factory.ts
@@ -1,15 +1,29 @@
 import { BuildingRequirement, Factory, FactoryDependency } from '@/interfaces/planner/FactoryInterface'
 
-export const findFac = (factoryId: string | number, factories: Factory[]): Factory | null => {
+export const findFac = (factoryId: string | number, factories: Factory[]): Factory => {
+  // This should always be supplied, if not there's a major bug.
   if (!factoryId) {
-    console.warn('No factoryId provided to findFactory')
-    return null
+    throw new Error('No factoryId provided to findFac')
   }
 
   // Ensure factoryId is parsed to a number to match factories array ids
   const factory = factories.find(fac => fac.id === parseInt(factoryId.toString(), 10))
   if (!factory) {
     throw new Error(`Factory ${factoryId} not found!`)
+  }
+  return factory
+}
+
+export const findFacByName = (name: string, factories: Factory[]): Factory => {
+  // This should always be supplied, if not there's a major bug.
+  if (!name) {
+    throw new Error(`No name provided to findFacByName`)
+  }
+
+  // Ensure factoryId is parsed to a number to match factories array ids
+  const factory = factories.find(fac => fac.name === name)
+  if (!factory) {
+    throw new Error(`Factory ${name} not found!`)
   }
   return factory
 }
