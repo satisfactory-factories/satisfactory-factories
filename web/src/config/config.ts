8e8a47671d373a991be86fc91d1e964ab78ca303
Sam Smith <samsmithnz@github.com>
2024-11-21 17:13:39 +0000

    Updating game-data in web.

--- web/src/config/config.ts
+++ web/src/config/config.ts
@@ -1,4 +1,4 @@
 export const config = {
   apiUrl: import.meta.env.VITE_ENV === 'dev' ? 'http://localhost:3001' : 'https://api.satisfactory-factories.app',
-  dataVersion: '1.0-12',
+  dataVersion: '1.0-13',
 }
