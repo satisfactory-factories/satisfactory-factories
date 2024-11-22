8eaf16bfc377a8bac63f65c139ab81c09d31f002
Sam Smith <samsmithnz@github.com>
2024-11-22 16:18:56 +0000

    Added base testing for parsing

---
+++ parsing/tests/common.spec.ts
@@ -0,0 +1,30 @@
+import { beforeEach, describe, expect, test } from '@jest/globals'
+// import { Factory } from '@/interfaces/planner/FactoryInterface'
+// import { createNewPart } from '@/utils/factory-management/common'
+// import { newFactory } from '@/utils/factory-management/factory'
+// import { addProductToFactory } from '@/utils/factory-management/products'
+
+describe('common', () => {
+//   let mockFactory: Factory
+
+//   beforeEach(() => {
+//     mockFactory = newFactory('Test Factory')
+//     addProductToFactory(mockFactory, {
+//       id: 'CompactedCoal',
+//       amount: 1234,
+//       recipe: 'CompactedCoal',
+//     })
+//   })
+
+  describe('generic test', () => {
+    test('should create a new test', () => {
+        //arrange
+
+        //act
+
+        //assert
+        expect(true).toBe(true);
+    })
+
+  })
+})
