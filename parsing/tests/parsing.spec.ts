7ea5dbf5fe26442a0d7ad78a057d53075afdf7e5
Sam Smith <samsmithnz@github.com>
2024-11-23 00:05:50 +0000

    fix: Added more building and power tests

--- parsing/tests/parsing.spec.ts
+++ parsing/tests/parsing.spec.ts
@@ -42,15 +42,49 @@ describe('common', () => {
         expect(results.recipes.length).toBe(293);
     })
 
+    
+
+    test('recipe ingredients and products test', () => {
+      //arrange
+
+      //act
+
+      //assert
+      // verify that each recipe has ingredients and products that exist in the parts list
+      console.log(results.recipes[0].ingredients)
+      // for (const recipe of results.recipes) {
+      //   for (const ingredient of recipe.ingredients) {
+      //     const part = results.items.parts[ingredient.part];
+      //     expect(part).toBeDefined();
+      //   }
+      //   for (const product of recipe.products) {
+      //     const part = results.items.parts[product.part];
+      //     expect(part).toBeDefined();
+      //   }
+      // }
+  })
+
     test('buildings test', () => {
         //arrange
 
         //act
-        console.log('buildings:');
+        // console.log('buildings:');
         console.log(results.buildings);
 
         //assert
         expect(Object.keys(results.buildings).length).toBe(12);
+        expect(results.buildings['assemblermk1']).toBe(15);
+        expect(results.buildings['blender']).toBe(75);
+        expect(results.buildings['constructormk1']).toBe(4);
+        expect(results.buildings['converter']).toBe(0.1); //TODO: This isn't right, it has a variable power consumption
+        expect(results.buildings['foundrymk1']).toBe(16);
+        expect(results.buildings['hadroncollider']).toBe(0.1); //TODO: This isn't right, it has a variable power consumption
+        expect(results.buildings['manufacturermk1']).toBe(55);
+        expect(results.buildings['nuclearpowerplant']).toBe(0); // Nuclear Power Generates power, it doesn't consume
+        expect(results.buildings['oilrefinery']).toBe(30);
+        expect(results.buildings['packager']).toBe(10);
+        expect(results.buildings['quantumencoder']).toBe(0.1); //TODO: This isn't right, it has a variable power consumption
+        expect(results.buildings['smeltermk1']).toBe(4);
     })
 
   })
