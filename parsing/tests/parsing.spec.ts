a51f53d00daded27a8a68768a045fc65bbfec260
Sam Smith <samsmithnz@github.com>
2024-11-23 12:18:09 +0000

    fix: code gardening

--- parsing/tests/parsing.spec.ts
+++ parsing/tests/parsing.spec.ts
@@ -1,8 +1,5 @@
 import { beforeAll, describe, expect, it, test } from '@jest/globals'
 import {processFile} from '@/processor'
-// import { createNewPart } from '@/utils/factory-management/common'
-// import { newFactory } from '@/utils/factory-management/factory'
-// import { addProductToFactory } from '@/utils/factory-management/products'
 
 describe('common', () => {
     let results: any;
@@ -41,78 +38,6 @@ describe('common', () => {
             expect(results.recipes.length).toBe(293);
         })
 
-
-        // test('recipe ingredients and products test', () => {
-        //     //arrange
-
-        //     //act
-
-        //     //assert
-        //     //console.log(results.items.parts);
-        //     // verify that each recipe has ingredients and products that exist in the parts list
-        //     for (const recipe of results.recipes) {
-
-        //         // for each recipe, check that each ingredient exists in the parts list
-        //         for (const ingredient of recipe.ingredients) {
-        //             //console.log(ingredient.part);
-        //             //console.log(recipe.ingredients);
-        //             const partName = results.items.parts[ingredient.part].name;
-                    
-        //             // Check if the ingredient exists in the parts list
-        //             if (partName in results.items.parts) {
-        //                 expect(partName).toBeDefined();
-        //             } else {
-        //                 //console.error(`Part ${ingredient.part} not found in parts list`);
-        //                 expect(`Part ${ingredient.part} not found in parts list`).toBe("");
-        //             }
-        //         }
-
-        //         // for each recipe, check that each product exists in the parts list
-
-
-
-
-
-        //     //   for (const ingredient of recipe.ingredients) {
-                
-        //     //     const partName = ingredient.part;
-        //     //     // Check if the part name exists in the parts list
-        //     //     if (partName in results.items.parts) {
-        //     //       const part = results.items.parts[partName];
-        //     //       console.log(`Part key: ${partName}, Part details:`, part);
-        //     //       expect(part).toBeDefined();
-        //     //     } else {
-        //     //       console.error(`Part ${partName} not found in parts list`);
-        //     //       expect(`Part ${partName} not found in parts list`).toBe("");
-        //     //     }
-        //     //   }
-        //         // for (const ingredient of recipe.ingredients) {
-        //         //     const part = results.items.parts[ingredient.part];
-        //         //     // Check if the ingredient exists in the parts list
-        //         //     if (part in results.items.parts) {
-        //         //         expect(results.items.parts[part]).toBeDefined();
-        //         //     } else if (part.name !== "Gift" && part.name !== "Snow") {
-        //         //       console.log("recipe:");
-        //         //       console.log(recipe);
-        //         //       console.log("part:");
-        //         //       console.log(part);
-        //         //       console.log("ingredient.part:");
-        //         //       console.log(ingredient.part);
-        //         //       console.log("finished");
-        //         //       expect(`In recipe '${recipe.displayName}', ingredient '${ingredient.part}' was not found in the parts list`).toBe("");
-        //         //     }
-        //         // }
-        //         for (const product of recipe.products) {
-        //             const part = results.items.parts[product.part];
-        //             // Check if the product exists in the parts list
-        //             if (part in Object.keys(results.items.parts)) {
-        //                 expect(results.items.parts[part]).toBeDefined();
-        //             } else if (part.name !== "Gift" && part.name !== "Snow") {
-        //                 expect(`In recipe '${recipe.displayName}', product '${product.part}' was not found in the parts list`).toBe("");
-        //               }
-        //         }
-        //     }
-        // })
 
         test('buildings test', () => {
             //arrange
