af285b64d4cb2157eda4a34b8c3b8e54133706f6
Matt Cavanagh <1776058+Maelstromeous@users.noreply.github.com>
2024-11-27 01:29:34 +0000

    Fix quantity inputs (#202)
    * fix(web): Added min widths to qty/min fields when >sm
    
    * fix(web): Removed commented out code that did nothing

--- web/src/components/planner/PlannerFactoryImports.vue
+++ web/src/components/planner/PlannerFactoryImports.vue
@@ -29,7 +29,6 @@
         </v-card-text>
       </v-card>
 
-      <!-- class="px-4 py-3 my-2 mx-0 align-center selectors sub-card border-md rounded" -->
       <div
         v-for="(input, inputIndex) in factory.inputs"
         :key="`${inputIndex}-${input.outputPart}`"
@@ -80,6 +79,7 @@
             hide-details
             label="Qty /min"
             :max-width="smAndDown ? undefined : '110px'"
+            :min-width="smAndDown ? undefined : '100px'"
             type="number"
             variant="outlined"
             @input="updateFactory(factory)"
