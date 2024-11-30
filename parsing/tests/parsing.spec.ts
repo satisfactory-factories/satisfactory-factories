8da38d7e88a8cdd846d3b4b6650e044a334c5f78
Sam Smith <samsmithnz@github.com>
2024-11-30 02:52:50 +0000

    fix: code gardening

--- parsing/tests/parsing.spec.ts
+++ parsing/tests/parsing.spec.ts
@@ -256,7 +256,7 @@ describe('common', () => {
         });
 
 
-        it('should create a biomass power generation recipe (solid biomass)', () => {
+        it('validate a biomass power generation recipe (solid biomass)', () => {
             //arrange
             const recipe : Recipe = results.powerGenerationRecipes.find((item: { id: string; }) => item.id === 'GeneratorBiomass_Automated_Biofuel');
 
