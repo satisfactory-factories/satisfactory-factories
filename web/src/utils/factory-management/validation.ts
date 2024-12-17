50e9c98a67f6c0419649d02bac1f9deade2fee8e
Matt Cavanagh <1776058+Maelstromeous@users.noreply.github.com>
2024-12-17 20:53:14 +0000

    feat(web): Describe more helpful detail to the user

--- web/src/utils/factory-management/validation.ts
+++ web/src/utils/factory-management/validation.ts
@@ -1,19 +1,18 @@
 // Check for invalid factory data e.g. inputs without factories etc
 import { findFac } from '@/utils/factory-management/factory'
 import { Factory } from '@/interfaces/planner/FactoryInterface'
+import { getPartDisplayName } from '@/utils/helpers'
 
 export const validateFactories = (factories: Factory[]) => {
   let hasErrors = false
 
   factories.forEach(factory => {
-    console.log('Validating factory:', factory.name)
-
     factory.inputs.forEach(input => {
       try {
         findFac(Number(input.factoryId), factories)
       } catch (err) {
         hasErrors = true
-        console.error(`Factory "${factory.name}" (${factory.id}) has an input for ${input.factoryId} with part ${input.outputPart} where which the factory does not exist!`)
+        console.error(`VALIDATION ERROR: Factory "${factory.name}" (${factory.id}) has an input for ${input.factoryId} with part ${input.outputPart} where which the factory does not exist!`)
 
         // Remove the input
         const index = factory.inputs.findIndex(i => i.factoryId === input.factoryId)
@@ -29,7 +28,13 @@ export const validateFactories = (factories: Factory[]) => {
         findFac(depFacId, factories)
       } catch (error) {
         hasErrors = true
-        console.error(`Factory "${factory.name}" (${factory.id}) has a dependency for ${depFacId} which does not exist!`)
+        console.error(`VALIDATION ERROR: Factory "${factory.name}" (${factory.id}) has a dependency for factory ID ${depFacId} which does not exist!`)
+
+        const requests = factory.dependencies.requests[depFacId]
+        // Loop the requests and split out the parts
+        requests.forEach(request => {
+          console.error(`Part ${request.part} with amount ${request.amount}`)
+        })
 
         // Remove the dependency
         delete factory.dependencies.requests[depFacId]
@@ -38,6 +43,6 @@ export const validateFactories = (factories: Factory[]) => {
   })
 
   if (hasErrors) {
-    alert('There were errors loading your factory data. Please check the browser console for more details. Firefox: Control + Shift + K, Chrome: Control + Shift + J')
+    alert('There were errors loading your factory data. Please check the browser console for more details. Firefox: Control + Shift + K, Chrome: Control + Shift + J. Look for "VALIDATION ERROR:".')
   }
 }
