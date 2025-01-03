0d32680f924d12ebf943880e625448bf4f874c2b
Matt Cavanagh <1776058+Maelstromeous@users.noreply.github.com>
2025-01-03 03:15:59 +0000

    feat(backend): Add date registered to user data

--- backend/models/UsersSchema.ts
+++ backend/models/UsersSchema.ts
@@ -4,6 +4,7 @@ const UserSchema = new mongoose.Schema(
   {
     username: { type: String, unique: true, required: true },
     password: { type: String, required: true },
+    registered: { type: Date, default: Date.now },
   }
 );
 export const User = mongoose.model('User', UserSchema);
