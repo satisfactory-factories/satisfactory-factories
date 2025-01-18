b2e26f94f4da2c3e385d2cf733fb2805804199aa
Matt Cavanagh <1776058+Maelstromeous@users.noreply.github.com>
2025-01-18 00:06:30 +0000

    feat(web): Added ability to dismiss the too many factories warning until refresh.

--- web/src/components/planner/PlannerTooManyFactoriesOpen.vue
+++ web/src/components/planner/PlannerTooManyFactoriesOpen.vue
@@ -1,9 +1,10 @@
 <template>
-  <div v-if="showAlert" class="count-warning bg-amber w-100 text-center py-1 position-absolute">
+  <div v-if="showAlert && !warningDismissed" class="count-warning bg-amber w-100 text-center py-1 position-absolute">
     <span v-if="tooManyShownFactories"><b>Too many factories are visible (> {{ factoryLimit }})</b>&nbsp;</span>
     <span v-if="tooManyInputs"><b>Too many imports are being shown! (> {{ inputLimit }})</b>&nbsp;</span>
     Please hide some factories or your planner will MAJORLY lag!
-    <v-btn density="compact" @click="emit('hide-all')">Hide all</v-btn>
+    <v-btn density="compact" @click="hideAll">Hide all</v-btn>
+    <v-btn class="ml-2 super-sm" density="compact" @click="warningDismissed = true">X</v-btn>
   </div>
 </template>
 
@@ -17,6 +18,7 @@
 
   const factoryLimit = 10
   const inputLimit = 30
+  const warningDismissed = ref(false)
 
   const emit = defineEmits<{(event: 'hide-all'): void}>()
   const inputs = computed(() => {
@@ -38,10 +40,21 @@
   const tooManyShownFactories = computed(() => openFactories.value > factoryLimit)
   const showAlert = computed(() => tooManyInputs.value || tooManyShownFactories.value)
 
+  const hideAll = () => {
+    warningDismissed.value = false
+    emit('hide-all')
+  }
+
 </script>
 
 <style lang="scss">
   .count-warning {
     z-index: 1;
+  }
+  .super-sm {
+    min-width: 30px;
+    width: 30px;
+    max-width: 30px;
+    padding: 0 2px;
   }
 </style>
